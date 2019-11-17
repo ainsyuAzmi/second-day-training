a23d435f6338947405218fb5ec6a26687caa4dc2
Ainsyu Azmi <ainsyu.azmi97@gmail.com>
2019-11-17 17:31:06 +0800

    Add master.blade.php

--- resources/views/template/home.blade.php
+++ resources/views/template/home.blade.php
@@ -1,195 +1,44 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.master')
 
-<head>
+@section('content')
+    @foreach($blogs as $blog)
+    <!-- Projects Section -->
+     <section id="projects" class="projects-section bg-light">
+     <div class="container">
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="">
-  <meta name="author" content="">
+       <!-- Featured Project Row -->
+       <div class="row align-items-center no-gutters mb-4 mb-lg-5">
+         <div class="col-xl-8 col-lg-7">
+           <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
+         </div>
+         <div class="col-xl-4 col-lg-5">
+           <div class="featured-text text-center text-lg-left">
+             <h4>{{ $blog->title}}</h4>
+             <p class="text-black-50 mb-0">{{ $blog->body}}</p>
+             <p class="text-black-50 mb-0">Created at {{ $blog->created_at->diffForHumans()}}</p>
+           </div>
+         </div>
+       </div>
 
-  <title>Grayscale - Start Bootstrap Theme</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom fonts for this template -->
-  <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/grayscale.min.css" rel="stylesheet">
-
-</head>
-
-<body id="page-top">
-
-  <!-- Navigation -->
-  <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-    <div class="container">
-      <a class="navbar-brand js-scroll-trigger" href="#page-top">Hello Ainsyu's World</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        Menu
-        <i class="fas fa-bars"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#about">About</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#projects">Projects</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="#signup">Contact</a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-
-
-  <!-- Header -->
-  <header class="masthead">
-    <div class="container d-flex h-100 align-items-center">
-      <div class="mx-auto text-center">
-        <h1 class="mx-auto my-0 text-uppercase">Laravel Training</h1>
-        <h2 class="text-white-50 mx-auto mt-2 mb-5">For UiTM Laravel Training Day 3</h2>
-        <a href="#about" class="btn btn-primary js-scroll-trigger">Get Started</a>
-      </div>
-    </div>
-  </header>
-
-  <!-- About Section -->
-  <section id="about" class="about-section text-center">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-8 mx-auto">
-          <h2 class="text-white mb-4">A 3 day lab training for Laravel Framework. Learnt a lot of valuable things from </h2>
-          <p class="text-white-50">
-            <a href="https://github.com/samtarmizi">samtarmizi</a>. Gonna learn tons of stuff before the day end.</p>
-        </div>
-      </div>
-      <img src="img/ipad.png" class="img-fluid" alt="">
-    </div>
-  </section>
-
-
-@foreach($blogs as $blog)
-  <!-- Projects Section -->
-  <section id="projects" class="projects-section bg-light">
-    <div class="container">
-
-      <!-- Featured Project Row -->
-      <div class="row align-items-center no-gutters mb-4 mb-lg-5">
-        <div class="col-xl-8 col-lg-7">
-          <img class="img-fluid mb-3 mb-lg-0" src="img/bg-masthead.jpg" alt="">
-        </div>
-        <div class="col-xl-4 col-lg-5">
-          <div class="featured-text text-center text-lg-left">
-            <h4>{{ $blog->title}}</h4>
-            <p class="text-black-50 mb-0">{{ $blog->body}}</p>
-            <p class="text-black-50 mb-0">{{ $blog->created_at->diffForHumans()}}</p>
-          </div>
-        </div>
-      </div>
-  </section>
-@endforeach
-
-  <!-- Signup Section -->
-  <section id="signup" class="signup-section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-10 col-lg-8 mx-auto text-center">
-
-          <i class="far fa-paper-plane fa-2x mb-2 text-white"></i>
-          <h2 class="text-white mb-5">Subscribe to receive updates!</h2>
-
-          <form class="form-inline d-flex">
-            <input type="email" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputEmail" placeholder="Enter email address...">
-            <button type="submit" class="btn btn-primary mx-auto">Subscribe</button>
-          </form>
-
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <!-- Contact Section -->
-  <section class="contact-section bg-black">
-    <div class="container">
-
-      <div class="row">
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-map-marked-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Address</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">4923 Market Street, Orlando FL</div>
+       <!-- Project One Row -->
+       <div class="row justify-content-center no-gutters mb-5 mb-lg-0">
+         <div class="col-lg-6">
+           <img class="img-fluid" src="img/demo-image-01.jpg" alt="">
+         </div>
+         <div class="col-lg-6">
+           <div class="bg-black text-center h-100 project">
+             <div class="d-flex h-100">
+               <div class="project-text w-100 my-auto text-center text-lg-left">
+                 <h4 class="text-white">Misty</h4>
+                 <p class="mb-0 text-white-50">An example of where you can put an image of a project, or anything else, along with a description.</p>
+                 <hr class="d-none d-lg-block mb-0 ml-0">
+               </div>
+             </div>
             </div>
-          </div>
-        </div>
+         </div>
+       </div>
+     </section>
 
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-envelope text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Email</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">
-                <a href="#">hello@yourdomain.com</a>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div class="col-md-4 mb-3 mb-md-0">
-          <div class="card py-4 h-100">
-            <div class="card-body text-center">
-              <i class="fas fa-mobile-alt text-primary mb-2"></i>
-              <h4 class="text-uppercase m-0">Phone</h4>
-              <hr class="my-4">
-              <div class="small text-black-50">+1 (555) 902-8832</div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div class="social d-flex justify-content-center">
-        <a href="#" class="mx-2">
-          <i class="fab fa-twitter"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-facebook-f"></i>
-        </a>
-        <a href="#" class="mx-2">
-          <i class="fab fa-github"></i>
-        </a>
-      </div>
-
-    </div>
-  </section>
-
-  <!-- Footer -->
-  <footer class="bg-black small text-center text-white-50">
-    <div class="container">
-      Copyright &copy; Your Website 2019
-    </div>
-  </footer>
-
-  <!-- Bootstrap core JavaScript -->
-  <script src={{ url("vendor/jquery/jquery.min.js") }}></script>
-  <script src={{ url("vendor/bootstrap/js/bootstrap.bundle.min.js") }}></script>
-
-  <!-- Plugin JavaScript -->
-  <script src={{ url("vendor/jquery-easing/jquery.easing.min.js") }}></script>
-
-  <!-- Custom scripts for this template -->
-  <script src={{ url("js/grayscale.min.js") }}></script>
-
-</body>
-
-</html>
+  
+    @endforeach
+@endsection
